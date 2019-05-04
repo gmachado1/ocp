7c9ac52992a9150c34f0085d21d438ce1cb2fafc
gmachado1 <g.machado1@gmail.com>
2019-05-04 11:49:21 -0300

    parte de Strings e localization

--- src/br/com/chap04/exam/Exame.java
+++ src/br/com/chap04/exam/Exame.java
@@ -1,6 +1,13 @@
 package br.com.chap04.exam;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.OptionalLong;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import java.util.stream.DoubleStream;
+import java.util.stream.IntStream;
+import java.util.stream.LongStream;
 import java.util.stream.Stream;
 
 public class Exame {
@@ -19,7 +26,36 @@ public class Exame {
 			System.out.println(b1 + " " + b2);
 		}
 		{
-			
+			LongStream ls = LongStream.of(1, 2, 3);
+			OptionalLong opt = ls.map(n -> n * 10).filter(n -> n <= 10).findFirst();
+			if (opt.isPresent()) {
+				System.out.println(opt.getAsLong());
+			}
+			opt.ifPresent(System.out::print);
+			System.out.println();
+
+		}
+		{
+			System.out.println(Stream.iterate(1, x -> ++x).limit(5).map(x -> "" + x).collect(Collectors.joining()));
+		}
+		{
+			List<Integer> l1 = Arrays.asList(1, 2, 3);
+			List<Integer> l2 = Arrays.asList(4, 5, 6);
+			List<Integer> l3 = Arrays.asList();
+			// Stream.of(l1, l2, l3).map(x-> x + 1).flatMap(x -> x.stream());
+			// System.out.println(c);
+		}
+		{
+			Stream<Integer> s = Stream.of(1);
+			IntStream is = s.mapToInt(x -> x);
+			DoubleStream ds = is.mapToDouble(x -> x);
+			Stream s2 = ds.mapToObj(x -> x);
+			s2.forEach(System.out::print);
+		}
+		{
+			DoubleStream s = DoubleStream.of(1.2, 2.4);
+			long d = s.peek(System.out::println).filter(x -> x > 2).count();
+			System.out.println(d);
 		}
 
 	}
