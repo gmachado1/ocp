7c1575dbf2e4594ab20694a33f236651a4ed16fa
gmachado1 <g.machado1@gmail.com>
2019-06-02 10:02:09 -0300

    finalizando Generics and Collections

--- src/br/com/genericsAndCollections/newJava8/Java8newFunc.java
+++ src/br/com/genericsAndCollections/newJava8/Java8newFunc.java
@@ -3,6 +3,7 @@ package br.com.genericsAndCollections.newJava8;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.function.BiFunction;
+import java.util.function.Function;
 
 public class Java8newFunc {
 
@@ -56,5 +57,34 @@ public class Java8newFunc {
 			favorites.merge("Sam", "Skyride", mapper);
 			System.out.println(favorites); // {Tom=Bus Tour, Sam=Skyride}
 		}
+		{
+			System.out.println("\n\n BiFunction4");
+			Map<String, Integer> counts = new HashMap<>();
+			counts.put("Jenny", 1);
+			BiFunction<String, Integer, Integer> mapper = (k, v) -> v + 1;
+			Integer jenny = counts.computeIfPresent("Jenny", mapper);
+			Integer sam = counts.computeIfPresent("Sam", mapper);
+			System.out.println(counts); // {Jenny=2}
+			System.out.println(jenny); // 2
+			System.out.println(sam); // null
+		}
+		{
+			Map<String, Integer> counts = new HashMap<>();
+			counts.put("Jenny", 15);
+			counts.put("Tom", null);
+			Function<String, Integer> mapper = (k) -> 1;
+			Integer jenny = counts.computeIfAbsent("Jenny", mapper); // 15
+			Integer sam = counts.computeIfAbsent("Sam", mapper); // 1
+			Integer tom = counts.computeIfAbsent("Tom", mapper); // 1
+			System.out.println(counts); // {Tom=1, Jenny=15, Sam=1}
+		}
+		{
+			Map<String, Integer> counts = new HashMap<>();
+			counts.put("Jenny", 1);
+			counts.computeIfPresent("Jenny", (k, v) -> null);
+			counts.computeIfAbsent("Sam", k -> null);
+			System.out.println(counts); // {}
+		}
+		
 	}
 }
