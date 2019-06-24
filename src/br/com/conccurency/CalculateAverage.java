02cbc1b6ddeb1f46ef90fd582076d8fabeffb7c8
gmachado1 <g.machado1@gmail.com>
2019-06-24 13:01:00 -0300

    relembrando e revisando parte de concurrency

---
+++ src/br/com/conccurency/CalculateAverage.java
@@ -0,0 +1,13 @@
+package br.com.conccurency;
+
+public class CalculateAverage implements Runnable {
+	private double[] scores;
+
+	public CalculateAverage(double[] scores) {
+		this.scores = scores;
+	}
+
+	public void run() {
+		// Define work here
+	}
+}
