feafd46bd83208879bbb10c505785599527689d2
gmachado1 <g.machado1@gmail.com>
2019-06-30 21:18:52 -0300

    review io study

--- src/br/com/io/streams/PrintWriterSample.java
+++ src/br/com/io/streams/PrintWriterSample.java
@@ -3,7 +3,9 @@ package br.com.io.streams;
 import java.io.*;
 
 public class PrintWriterSample {
+
 	public static void main(String[] args) throws IOException {
+		
 		File source = new File("beach.log");
 		try (PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(source)))) {
 			out.print("Today's weather is: ");
